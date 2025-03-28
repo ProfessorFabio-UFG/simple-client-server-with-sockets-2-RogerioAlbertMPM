44d74cf620bcb24d595676e8f516fae285f8e507
RogerioAlbertMPM <rogerioalbertmozer@hotmail.com>
2025-03-28 18:23:24 -0300

    Código do Exercício

--- client.py
+++ client.py
@@ -3,7 +3,16 @@ from constCS import * #-
 
 s = socket(AF_INET, SOCK_STREAM)
 s.connect((HOST, PORT)) # connect to server (block until accepted)
-s.send(str.encode('Hello, world'))  # send some data
-data = s.recv(1024)     # receive the response
-print (bytes.decode(data))            # print the result
+
+
+while True:
+    inp = input("Commando: uppercase/lowercase/reverse texto OU 'q' para sair ")
+
+    if inp.lower() == 'q':
+        break
+    
+    s.send(str.encode(inp))
+    data = s.recv(1024)     # receive the response
+    print ("Servidor: ", bytes.decode(data))            # print the result
+
 s.close()               # close the connection
--- server.py
+++ server.py
@@ -1,13 +1,41 @@
 from socket  import *
 from constCS import * #-
 
+
+
+def process(cmd, text):
+    if cmd == 'uppercase':
+        return text.upper()
+    elif cmd == 'lowercase':
+        return text.lower()
+    elif cmd == 'reverse':
+        return text[::-1]
+    else:
+        return "Erro: Comando desconhecido"
+    
+
 s = socket(AF_INET, SOCK_STREAM) 
 s.bind((HOST, PORT))  #-
 s.listen(1)           #-
+print(f"Servidor de Texto ativo em {HOST}:{PORT}...")
+
 (conn, addr) = s.accept()  # returns new socket and addr. client 
+print(f"Conexão recebida de {addr}")
+
 while True:                # forever
   data = conn.recv(1024)   # receive data from client
   if not data: break       # stop if client stopped
-  print(bytes.decode(data))
-  conn.send(str.encode(bytes.decode(data)+"*")) # return sent data plus an "*"
+
+  msg = bytes.decode(data)
+  print(f"Recebido: {msg}")
+  
+  command_struct = msg.strip().split(maxsplit=1)
+
+  if len(command_struct) < 2:
+      ans = "Erro: formato esperado é 'uppercase/lowercase/reverse texto"
+  else:
+      cmd, text = command_struct
+      ans = process(cmd, text)
+
+  conn.send(str.encode(ans+"*")) # return sent data plus an "*"
 conn.close()               # close the connection
